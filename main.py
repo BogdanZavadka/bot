4b674acb53fa10c4d891a4a0ad100c9de63daff1
BogdanZavadka <bogdanzavadka@gmail.com>
2022-10-23 23:30:09 +0300

    added inserts to a database and some functionality for private chats

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import telebot
+from telebot import types
 import psycopg2
 
 bot = telebot.TeleBot('5728154756:AAE6bRH8TuCDACbrUkxHBidCwPd348MyAtY')
@@ -11,19 +12,49 @@ db_name = "yxqfwklx"
 @bot.message_handler(commands=['start'])
 def start(message):
     if message.chat.type != 'group':
-        bot.send_message(message.chat.id, 'Add this bot to your chat')
+        markup = types.InlineKeyboardMarkup()
+        markup.add(types.InlineKeyboardButton('Bot info', callback_data='info'),
+                   types.InlineKeyboardButton('Website', url='https://www.youtube.com/c/STERNENKO'))
+        bot.send_message(message.chat.id, 'Hello. This bot designed only for groups', reply_markup=markup)
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callbacks(call):
+    if call.data == 'info':
+        markup = types.InlineKeyboardMarkup()
+        markup.add(types.InlineKeyboardButton('Bot info', callback_data='info'),
+                   types.InlineKeyboardButton('Website', url='https://www.youtube.com/c/STERNENKO'))
+        bot.send_message(call.message.chat.id, 'This bot can help you detect ads in your group.'
+                         ' Just add this bot to a group and give it admin rights.', reply_markup=markup)
+        bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.id, reply_markup=None,
+                              text='Hello. This bot designed only for groups')
 
 
 @bot.message_handler(content_types=['text'])
 def save_message(message):
     if 'https' in message.text or 'http' in message.text or '.com' in message.text:
-        bot.send_message(message.chat.id, 'Your message contains a link', reply_to_message_id=message.id)
+        bot.send_message(message.chat.id, message)
+        bot.send_message(message.chat.id, f'@{message.from_user.username}, your message seem to contain a link',
+                         reply_to_message_id=message.id)
+        connection = psycopg2.connect(host=host, user=user, password=password, database=db_name)
+        group_ids = []
         try:
-            connection = psycopg2.connect(host=host, user=user, password=password, database=db_name)
             with connection.cursor() as cursor:
+                cursor.execute('SELECT group_id FROM tg_group;')
+                group_ids = cursor.fetchall()
+                print(group_ids)
+        except Exception as ex:
+            print('Error ', ex)
+
+        try:
+            with connection.cursor() as cursor:
+                if not group_ids:
+                    cursor.execute(f"INSERT INTO tg_group VALUES ('{message.chat.id}', '{message.chat.title}');")
+                cursor.execute(f'INSERT INTO tg_user(user_id, username) VALUES ({message.from_user.id}, '
+                               f"'{message.from_user.username}');")
                 cursor.execute(
-                    f"INSERT INTO test_table (messag) VALUES ('{message.text}');"
-                )
+                    f"INSERT INTO tg_message VALUES ({message.id}, '{message.text}', {message.chat.id}, "
+                    f"{message.from_user.id});")
                 connection.commit()
                 # cursor.execute('SELECT * FROM test_table;')
                 # a = cursor.fetchall()
@@ -33,8 +64,8 @@ def save_message(message):
                 # print(b)
                 # bot.send_message(message.chat.id, b)
         except Exception as _ex:
-            print("[NFO] Error", _ex)
+            print("[INFO] Error", _ex)
+        connection.close()
 
 
 bot.polling(none_stop=True)
-
