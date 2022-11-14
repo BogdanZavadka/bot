fcf0008cea634d76e75ce071c7ebd9eefbdad1c0
BogdanZavadka <bogdanzavadka@gmail.com>
2022-11-14 15:48:23 +0200

    connected to API, updated requirements for heroku

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import telebot
 from telebot import types
-import psycopg2
+import requests
 
 bot = telebot.TeleBot('5728154756:AAE6bRH8TuCDACbrUkxHBidCwPd348MyAtY')
 host = "ella.db.elephantsql.com"
 user = "yxqfwklx"
 password = "cWmYO0EkdU_nDVvGgWXKsJEf4RQLSR8F"
 db_name = "yxqfwklx"
+
+base_adress = 'https://glacial-earth-93029.herokuapp.com/'
 
 
 @bot.message_handler(commands=['start'])
@@ -25,7 +27,8 @@ def callbacks(call):
         markup.add(types.InlineKeyboardButton('Bot info', callback_data='info'),
                    types.InlineKeyboardButton('Website', url='https://www.youtube.com/c/STERNENKO'))
         bot.send_message(call.message.chat.id, 'This bot can help you detect ads in your group.'
-                         ' Just add this bot to a group and give it admin rights.', reply_markup=markup)
+                                               ' Just add this bot to a group and give it admin rights.',
+                         reply_markup=markup)
         bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.id, reply_markup=None,
                               text='Hello. This bot designed only for groups')
 
@@ -33,39 +36,38 @@ def callbacks(call):
 @bot.message_handler(content_types=['text'])
 def save_message(message):
     if 'https' in message.text or 'http' in message.text or '.com' in message.text:
-        bot.send_message(message.chat.id, message)
-        bot.send_message(message.chat.id, f'@{message.from_user.username}, your message seem to contain a link',
+        bot.send_message(message.chat.id, f'@{message.from_user.username}, your message seems to contain a link',
                          reply_to_message_id=message.id)
-        connection = psycopg2.connect(host=host, user=user, password=password, database=db_name)
-        group_ids = []
-        try:
-            with connection.cursor() as cursor:
-                cursor.execute('SELECT group_id FROM tg_group;')
-                group_ids = cursor.fetchall()
-                print(group_ids)
-        except Exception as ex:
-            print('Error ', ex)
-
-        try:
-            with connection.cursor() as cursor:
-                if not group_ids:
-                    cursor.execute(f"INSERT INTO tg_group VALUES ('{message.chat.id}', '{message.chat.title}');")
-                cursor.execute(f'INSERT INTO tg_user(user_id, username) VALUES ({message.from_user.id}, '
-                               f"'{message.from_user.username}');")
-                cursor.execute(
-                    f"INSERT INTO tg_message VALUES ({message.id}, '{message.text}', {message.chat.id}, "
-                    f"{message.from_user.id});")
-                connection.commit()
-                # cursor.execute('SELECT * FROM test_table;')
-                # a = cursor.fetchall()
-                # b = ''
-                # for i in a:
-                #     b += str(i)
-                # print(b)
-                # bot.send_message(message.chat.id, b)
-        except Exception as _ex:
-            print("[INFO] Error", _ex)
-        connection.close()
+        name = message.from_user.first_name + ' ' + str(message.from_user.last_name)
+        user_payload = {
+            'userId': int(message.from_user.id),
+            'name': str(name),
+            'username': str(message.from_user.username),
+            'phoneNumber': 'None'
+        }
+        group_payload = {
+            'groupId': int(message.chat.id),
+            'name': str(message.chat.title)
+        }
+        message_payload = {
+            'messageId': int(message.message_id),
+            'isAdvertisement': 'null',
+            'messageBody': str(message.text),
+            'sendDate': message.date,
+            'group': {
+                'groupId': int(message.chat.id),
+                'name': str(message.chat.title)
+            },
+            'user': {
+                'userId': int(message.from_user.id),
+                'name': str(name),
+                'username': str(message.from_user.username),
+                'phoneNumber': 'null'
+            }
+        }
+        print(requests.post(base_adress + 'users/', json=user_payload))
+        print(requests.post(base_adress + 'groups/', json=group_payload))
+        print(requests.post(base_adress + 'messages/', json=message_payload))
 
 
 bot.polling(none_stop=True)
